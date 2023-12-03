eab07288eb468de16ec620a97275966d12ddff28
Andrew Ipu <ipuandrew@gmail.com>
2023-12-03 19:56:33 +0300

    string extraction using regex

--- Time Calculator/main.js
+++ Time Calculator/main.js
@@ -1,8 +1,15 @@
-//? 1. 
-//? 2. 
+//? 1. I need an array to hold the days of the week.
+//? 2. Main function will have three parameters. StartTime, duration, and an optional one called days.
+    //? days is there in case the user wants to give a day of the week in addition to the time.
+//? 3. To parse the input, Regex is advised.
+//? 4. Simply put - the function should add the duration time to the start time and return the result.
+//? 5. If the result lands on the next day then the output should indicate "next day".
+//? 6. If the result is several days later then the output should indicate "n days later"
+//? 7. Day of the week should appear after the time and before the number of days later.
+
 import { addTime } from "./time_calculator";
 
-console.log(addTime("11:59 PM", "24:05", "Wednesday"));
+console.log(addTime("3:30 PM", "2:12")); //should return 5:42 PM
 
 
 //export above functions
--- Time Calculator/time_calculator.js
+++ Time Calculator/time_calculator.js
@@ -1,5 +1,19 @@
 'use strict';
 
-export function addTime(){
+export function addTime(start, duration){
 
+    //extract the start hour
+    let startHour = start.match(/(\d\S*):/)[1];
+
+    //extract the start min
+    let startMin = start.match(/\d+(?= )/)[0];
+
+    //extract the AM/PM
+    let AM_PM = start.match(/\S[PM]/)[0];
+
+    //extract duration_hours
+    let durHour = duration.match(/\d\S*(?=:)/)[0];
+
+    //extract duration_minutes
+    let durMin = duration.match(/\d+\d+/)[0];
 }
